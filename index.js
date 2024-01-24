b4cb5d3cb0b05ebc32ccfb8bffe6f5993f7522e3
Vaibhav Patil <vaibhav.patil@contentstack.com>
2024-01-24 16:38:28 +0530

    Added server

---
+++ index.js
@@ -0,0 +1,18 @@
+var express = require("express");
+var app = express();
+app.use(express.json());
+
+const webhookData = {};
+app.get("/", function (req, res) {
+  res.json(webhookData);
+});
+
+app.post("/", function (req, res) {
+  webhookData.headers = req.headers;
+  webhookData.body = req.body;
+  res.send();
+});
+
+app.listen(3000, function () {
+  console.log("Example app listening on port 3000!");
+});
